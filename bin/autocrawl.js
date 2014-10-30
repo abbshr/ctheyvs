1ed754f7c94353bcb3f009616ca9b25abc52786e
Ran <abbshr@outlook.com>
2014-10-30 19:24:19 +0800

    添加expires

--- bin/autocrawl.js
+++ bin/autocrawl.js
@@ -5,24 +5,31 @@ var util = require('util');
 var dataCenter = require('../model');
 var tracker = require('../lib/tracker');
 
-dataCenter.iterAllLocations(function (err, result) {
-  if (err) {
-    throw err;
-  } else if (result) {
-    console.log('命中所有缓存地点, 开始更新数据...');
-    result.forEach(function (l) {
-      console.log('正在抓取地点 [', l, '] 周边餐饮信息...');
-      var tracker = new Tracker(l);
-      tracker.trackRestaurant(function (result) {
-        tracker.normalize(result, function (result) {
-          // 餐馆信息存入数据库
-          dataCenter.storeLocation(l, result, function () {
-            util.log(l + ' 周边数据已经是最新.');
+// 6 hours
+const EXPIRES = 1000 * 60 * 60 * 6;
+setInterval(sync, EXPIRES);
+sync();
+
+function sync() {
+  dataCenter.iterAllLocations(function (err, result) {
+    if (err) {
+      throw err;
+    } else if (result) {
+      console.log('命中所有缓存地点, 开始更新数据...');
+      result.forEach(function (l) {
+        console.log('正在抓取地点 [', l, '] 周边餐饮信息...');
+        var tracker = new Tracker(l);
+        tracker.trackRestaurant(function (result) {
+          tracker.normalize(result, function (result) {
+            // 餐馆信息存入数据库
+            dataCenter.storeLocation(l, result, function () {
+              util.log(l + ' 周边数据已经是最新.');
+            });
           });
         });
       });
-    });
-  } else {
-    // init
-  }
-});
+    } /*else {
+      // init
+    }*/
+  });
+}
